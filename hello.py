45d895d50917640c7f004aa9d3104c1899b55da2
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-08-07 16:17:47 -0400

    add a new route

--- hello.py
+++ hello.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-    print "wat:"
+
+
+@app.route('/whoami')
+def whoami():
+    return 'I am Michael D. Hoyle, 26 year old programmer, and lover of dogs.'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
