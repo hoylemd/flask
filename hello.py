58f2366e38c078ed95eaf3bec8c95bab8790c987
Michael D. Hoyle <mhoyle@freshbooks.com>
2015-08-07 16:25:17 -0400

    add prod mode running

--- hello.py
+++ hello.py
@@ -11,5 +11,10 @@ def hello_world():
 def whoami():
     return 'I am Michael D. Hoyle, 26 year old programmer, and lover of dogs.'
 
+
+def run_app():
+    app.run(host='0.0.0.0', debug=False)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
